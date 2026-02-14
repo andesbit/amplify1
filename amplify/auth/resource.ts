505f9bea517e7cc00677d1380ab0be000ef84fea
andesbit <adocarpel@gmail.com>
2026-02-14 17:28:14 -0500

    Add Google OAuth to production

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,7 +1,23 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+      },
+      callbackUrls: [
+        'http://localhost:3000/',
+        'http://localhost:3000/profile',
+        'https://main.d2npvh5w3g4srb.amplifyapp.com/',
+        'https://main.d2npvh5w3g4srb.amplifyapp.com/profile',
+      ],
+      logoutUrls: [
+        'http://localhost:3000/',
+        'https://main.d2npvh5w3g4srb.amplifyapp.com/',
+      ],
+    },
   },
 });
