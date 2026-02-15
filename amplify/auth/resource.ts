298d824cb831731fe3ffd1efe7666f3acf4b45d7
andesbit <adocarpel@gmail.com>
2026-02-15 08:12:10 -0500

    Add Google OAuth attribute mapping

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,3 @@
-
 import { defineAuth, secret } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
@@ -8,6 +7,12 @@ export const auth = defineAuth({
       google: {
         clientId: secret('GOOGLE_CLIENT_ID'),
         clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+        scopes: ['email', 'profile', 'openid'],
+        attributeMapping: {
+          email: 'email',
+          givenName: 'given_name',
+          familyName: 'family_name',
+        },
       },
       callbackUrls: [
         'http://localhost:3000/',
